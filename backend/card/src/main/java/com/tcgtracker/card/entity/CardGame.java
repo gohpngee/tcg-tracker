fb671149433129ef49c66a80290a5aa207bd60e9
Brian Goh <gohpngee@gmail.com>
2026-09-13 23:01:31 +0800

    fix some security vulnerability to remove access to setting attributes in this entity class

--- backend/card/src/main/java/com/tcgtracker/card/entity/CardGame.java
+++ backend/card/src/main/java/com/tcgtracker/card/entity/CardGame.java
@@ -1,5 +1,7 @@
 package com.tcgtracker.card.entity;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,19 +10,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "card_game")
-@NoArgsConstructor 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter 
-@Setter 
-@AllArgsConstructor 
-@Builder 
 public class CardGame {
     public enum GameName {
         POKEMON,
@@ -35,5 +32,8 @@ public class CardGame {
     @Column(nullable = false, unique = true)
     private GameName gameName;
 
-
+    //include constructor so that gameName cannot be null
+    public CardGame(GameName gameName) {
+        this.gameName = Objects.requireNonNull(gameName);
+    }
 }
