95d4b16f4cdfc1d07ac0923d36d45448e8e3307e
Brian Goh <gohpngee@gmail.com>
2026-09-17 17:58:58 +0800

    Add CardSetRepository for queries regarding CardSet entity as well as the persistence tests for it

---
+++ backend/card/src/main/java/com/tcgtracker/card/repository/CardSetRepository.java
@@ -0,0 +1,50 @@
+package com.tcgtracker.card.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+
+import com.tcgtracker.card.entity.CardSet;
+import com.tcgtracker.card.entity.CardGame;
+
+@Repository 
+public class CardSetRepository {
+    private final EntityManager entityManager;
+
+    public CardSetRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    public Optional<CardSet> findByExternalId(String externalId) {
+        //returning Optional.ofNullable because we are returning an Optional CardSet object, so need to use a wrapper
+        return Optional.ofNullable(entityManager.createQuery("""
+                SELECT cs FROM CardSet cs
+                WHERE cs.externalId = :externalId   
+                """, CardSet.class)
+                .setParameter("externalId", externalId)
+                .getSingleResultOrNull());
+    }
+
+    public Optional<CardSet> findBySetCode(String setCode) {
+        return Optional.ofNullable(entityManager.createQuery("""
+                SELECT cs FROM CardSet cs
+                WHERE cs.setCode = :setCode
+                """, CardSet.class)
+                .setParameter("setCode", setCode)
+                .getSingleResultOrNull());
+    }
+
+    public List<CardSet> findByCardGameId(Long cardGameId) {
+        return entityManager.createQuery("""
+                SELECT cs FROM CardSet cs
+                WHERE cs.cardGame.id = :cardGameId
+                """, CardSet.class)
+                .setParameter("cardGameId", cardGameId)
+                .getResultList();
+    }
+
+}
---
+++ backend/card/src/test/java/com/tcgtracker/card/repository/CardSetRepositoryPersistenceTest.java
@@ -0,0 +1,110 @@
+package com.tcgtracker.card.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+
+import com.tcgtracker.card.entity.CardSet.Language;
+import com.tcgtracker.card.entity.CatalogueSource;
+import com.tcgtracker.card.entity.CardGame;
+import com.tcgtracker.card.entity.CardSet;
+
+@DataJpaTest
+@ContextConfiguration(
+    classes = CardSetRepositoryPersistenceTest.JpaTestConfiguration.class
+)
+public class CardSetRepositoryPersistenceTest {
+    @Configuration(proxyBeanMethods = false)
+    @AutoConfigurationPackage(basePackages = "com.tcgtracker.card")
+    @EntityScan(basePackageClasses = CardSet.class)
+    @Import(CardSetRepository.class)
+    static class JpaTestConfiguration {}
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private CardSetRepository cardSetRepository;
+
+    CardGame cardGame = new CardGame(CardGame.GameName.POKEMON);
+
+    CardSet svBaseSet = new CardSet(
+        "sv01",
+        "SV01",
+        "Scarlet & Violet Base",
+        Language.ENGLISH,
+        cardGame,
+        LocalDate.of(2023, 3, 31),
+        258,
+        CatalogueSource.TCGDEX,
+        "https://www.tcgdex.net/images/sets/sv01.png"
+    );
+
+    CardSet paldeaEvolvedSet = new CardSet(
+        "sv02",
+        "SV02",
+        "Paldea Evolved",
+        Language.ENGLISH,
+        cardGame,
+        LocalDate.of(2023, 06, 9),
+        279,
+        CatalogueSource.TCGDEX,
+        "https://www.tcgdex.net/images/sets/sv02.png"
+    );
+
+    @Test
+    void findByExternalIdTest() {
+        entityManager.persist(cardGame);
+        entityManager.persist(svBaseSet);
+        entityManager.persist(paldeaEvolvedSet);
+
+        entityManager.flush();
+
+        String targetExternalId = paldeaEvolvedSet.getExternalId();
+        Optional<CardSet> result = cardSetRepository.findByExternalId(targetExternalId);
+
+        assertEquals(targetExternalId, result.get().getExternalId());
+    }
+
+    @Test
+    void findBySetCodeTest() {
+        entityManager.persist(cardGame);
+        entityManager.persist(svBaseSet);
+        entityManager.persist(paldeaEvolvedSet);
+
+        entityManager.flush();
+
+        String targetSetCode = paldeaEvolvedSet.getSetCode();
+        Optional<CardSet> result = cardSetRepository.findBySetCode(targetSetCode);
+
+        assertEquals(targetSetCode, result.get().getSetCode());
+    }
+
+    @Test
+    void findByCardGameIdTest() {
+        entityManager.persist(cardGame);
+        entityManager.persist(svBaseSet);
+        entityManager.persist(paldeaEvolvedSet);
+
+        entityManager.flush();
+
+        Long targetCardGameId = cardGame.getId();
+
+        List<CardSet> result = cardSetRepository.findByCardGameId(targetCardGameId);
+
+        assertEquals(2, result.size());
+    }
+
+}
